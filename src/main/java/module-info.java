629fadbbfaf1c42f5a9c5bb7082285a459653cef
Lorenzo Battilocchi <battilocchilorenzo98@gmail.com>
2026-06-12 22:38:06 +0200

    Integrate Inference Framework (#3)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module dev.xerohero.fixiy {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires langchain4j.embeddings;
 
 
     opens dev.xerohero.fixiy to javafx.fxml;
